69df3230f05d4c4d6b6311a5769504a751d77b10
Justin Thomas <jthomas0209@gmail.com>
2016-02-16 09:20:40 -0500

    Models done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    return "sad" if happiness < nausea
+    return "happy" if happiness > nausea
+  end
+
 end
